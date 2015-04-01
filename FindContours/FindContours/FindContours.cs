cb59b9f5f6296094e7e06e044d5c4a55f5b8af51
RomanDubinin <Roman230394@yandex.ru>
2015-04-02 03:57:33 +0600

    пока сложно задать интересующий цвет

---
+++ FindContours/FindContours/FindContours.cs
@@ -0,0 +1,70 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Drawing;
+/**
+ * Developed by : Lasitha Ishan Petthawadu
+ * Email        : petthawadu3@gmail.com
+ * Description  : http://wp.me/p429SV-8G
+ * Date : 12/06/2014
+ */
+namespace FindContours
+{
+    class FindContours
+    {
+        public void IdentifyContours(Bitmap actualImage,int thresholdValue1, int thresholdValue2, bool invert,out Bitmap processedGray,out Bitmap processedColor)
+        {
+
+            //1. Convert the image to grayscale.
+            
+            #region Conversion To grayscale
+			var actim = new Image<Bgr, byte>(actualImage).InRange(new Bgr(0, 0, 105), new Bgr(100, 100, 256));
+	        //processedColor = actim.Bitmap;
+            var grayImage = (actim);
+            var colorImage = new Image<Bgr, byte>(actualImage);
+	        //processedGray = grayImage.Bitmap;
+			//return;
+            
+            #endregion
+            
+            //2. Threshold it.
+            
+            #region  Image normalization and inversion (if required)
+
+			grayImage = grayImage.ThresholdBinaryInv(new Gray(thresholdValue1),new Gray(thresholdValue2));
+          
+            if (invert)
+            {
+                grayImage._Not();
+            }  
+            #endregion
+            
+            //3. Extract the contours.
+            
+            #region Extracting the Contours
+            using (MemStorage storage = new MemStorage())
+            {
+
+                for (Contour<Point> contours = grayImage.FindContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_TC89_L1, RETR_TYPE.CV_RETR_EXTERNAL, storage); contours != null; contours = contours.HNext)
+                {
+
+                    Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.015, storage);
+                    if (currentContour.BoundingRectangle.Width > 20)
+                    {
+                        CvInvoke.cvDrawContours(colorImage, contours, new MCvScalar(105), new MCvScalar(25), -1, 2, LINE_TYPE.EIGHT_CONNECTED, new Point(0, 0));
+                        colorImage.Draw(currentContour.BoundingRectangle, new Bgr(Color.DeepPink), 2);
+                    }
+                }
+
+            } 
+            #endregion
+            
+            //4. Setting the results to the output variables.
+
+            #region Asigning output
+            processedColor = colorImage.ToBitmap();
+            processedGray = grayImage.ToBitmap();
+            #endregion
+        }
+    }
+}
---
+++ FindContours/FindContours/frmContours.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+ * Developed by : Lasitha Ishan Petthawadu
+ * Email        : petthawadu3@gmail.com
+ * Description  : http://wp.me/p429SV-8G
+ * Date : 12/06/2014
+ */
+namespace FindContours
+{
+    /// <summary>
+    /// Main form to interact with EmguCV for contour processing.
+    /// </summary>
+    public partial class FrmContours : Form
+    {
+        Emgu.CV.Capture capture;
+        FindContours processor = new FindContours();
+        Bitmap colorImage;
+
+        /// <summary>
+        /// Initializes the initial tracker value to th tracker label.
+        /// </summary>
+        public FrmContours()
+        {
+            InitializeComponent();
+            lblThresholdValue1.Text = trackbarThreshold1.Value.ToString();
+            lblThresholdValue2.Text = trackbarThreshold2.Value.ToString();
+        }
+
+        /// <summary>
+        /// Updates the threshold label when the tracker changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackbarThreshold_Scroll1(object sender, EventArgs e)
+        {
+            lblThresholdValue1.Text = trackbarThreshold1.Value.ToString();
+        }
+
+		private void trackbarThreshold_Scroll2(object sender, EventArgs e)
+		{
+			lblThresholdValue2.Text = trackbarThreshold1.Value.ToString();
+		}
+
+        /// <summary>
+        /// Starts the camera capture.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+			if (capture == null)
+            {
+				capture = new Emgu.CV.Capture();
+            }
+            CameraStreamCapture.Enabled = true;
+
+        }
+
+        /// <summary>
+        /// Stops the Camera capture.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStopCapture_Click(object sender, EventArgs e)
+        {
+            CameraStreamCapture.Enabled = false;
+			if (capture != null)
+            {
+				capture.Dispose();
+				capture = null;
+            }
+
+        }
+        /// <summary>
+        /// Start capturing from the camera stream.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CameraStreamCapture_Tick(object sender, EventArgs e)
+        {
+			var qf = capture.QueryFrame();
+	        if (qf == null)
+	        {
+				return;
+	        }
+			var bm = qf.ToBitmap();
+			var image = bm;
+            Bitmap color;
+            Bitmap gray;
+			//image = new Bitmap("C:\\Users\\Roman\\Desktop\\Untitled3.png");
+			//image = new Bitmap("C:\\Emgu\\emgucv-windows-universal-cuda 2.4.10.1940\\bin\\pedestrian.png");
+			//image = new Bitmap("C:\\Emgu\\emgucv-windows-universal-cuda 2.4.10.1940\\bin\\stop-sign.jpg");
+            processor.IdentifyContours(image, trackbarThreshold1.Value, trackbarThreshold2.Value, chkBoxInvert.Checked, out gray, out color);
+            pictBoxColor.Image = color;
+            pictBoxGray.Image = gray;
+        }
+
+        /// <summary>
+        /// Stop the camera and release resources when the form is closing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmContours_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CameraStreamCapture.Enabled = false;
+			capture = null;
+        }
+    }
+}
