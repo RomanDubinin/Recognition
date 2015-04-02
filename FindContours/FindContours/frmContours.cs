580d66d21c5111f1a4a8898610c2063a9090adfd
RomanDubinin <Roman230394@yandex.ru>
2015-04-02 18:45:02 +0600

    промежуточный коммит

--- FindContours/FindContours/frmContours.cs
+++ FindContours/FindContours/frmContours.cs
@@ -1,125 +1,123 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using ObjectFinderCore;
 
-/**
- * Developed by : Lasitha Ishan Petthawadu
- * Email        : petthawadu3@gmail.com
- * Description  : http://wp.me/p429SV-8G
- * Date : 12/06/2014
- */
 namespace FindContours
 {
-    /// <summary>
-    /// Main form to interact with EmguCV for contour processing.
-    /// </summary>
-    public partial class FrmContours : Form
-    {
-        Emgu.CV.Capture capture;
-        FindContours processor = new FindContours();
-        Bitmap colorImage;
+	public partial class FrmContours : Form
+	{
+		private Capture capture;
+		private FindContours processor = new FindContours();
+		private ObjectFinder ObjectFinder;
 
-        /// <summary>
-        /// Initializes the initial tracker value to th tracker label.
-        /// </summary>
-        public FrmContours()
-        {
-            InitializeComponent();
-        }
+		public FrmContours()
+		{
+			InitializeComponent();
+			ObjectFinder = new ObjectFinder();
+		}
 
-        /// <summary>
-        /// Starts the camera capture.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnCapture_Click(object sender, EventArgs e)
-        {
+		/// <summary>
+		/// Starts the camera capture.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnCapture_Click(object sender, EventArgs e)
+		{
 			if (capture == null)
-            {
-				capture = new Emgu.CV.Capture(1);// cam num
-            }
-            CameraStreamCapture.Enabled = true;
+			{
+				capture = new Emgu.CV.Capture(1); // cam num
+			}
+			CameraStreamCapture.Enabled = true;
+		}
 
-        }
-
-        /// <summary>
-        /// Stops the Camera capture.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnStopCapture_Click(object sender, EventArgs e)
-        {
-            CameraStreamCapture.Enabled = false;
+		/// <summary>
+		/// Stops the Camera capture.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnStopCapture_Click(object sender, EventArgs e)
+		{
+			CameraStreamCapture.Enabled = false;
 			if (capture != null)
-            {
+			{
 				capture.Dispose();
 				capture = null;
-            }
+			}
+		}
 
-        }
-        /// <summary>
-        /// Start capturing from the camera stream.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CameraStreamCapture_Tick(object sender, EventArgs e)
-        {
+		/// <summary>
+		/// Start capturing from the camera stream.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CameraStreamCapture_Tick(object sender, EventArgs e)
+		{
 			var qf = capture.QueryFrame();
-	        if (qf == null)
-	        {
+			if (qf == null)
+			{
 				return;
-	        }
+			}
 			var bm = qf.ToBitmap();
 			var image = bm;
-            Bitmap color;
-            Bitmap gray;
+			var myEye = new Image<Bgr, byte>(image);
 			//image = new Bitmap("C:\\Users\\Roman\\Desktop\\Untitled3.png");
 			//image = new Bitmap("C:\\Emgu\\emgucv-windows-universal-cuda 2.4.10.1940\\bin\\pedestrian.png");
 			//image = new Bitmap("C:\\Emgu\\emgucv-windows-universal-cuda 2.4.10.1940\\bin\\stop-sign.jpg");
-            processor.IdentifyContours(image, trackBar1.Value, trackBar2.Value, trackBar3.Value, trackBar4.Value, trackBar5.Value, trackBar6.Value, chkBoxInvert.Checked, out gray, out color);
-            pictBoxColor.Image = color;
-            pictBoxGray.Image = gray;
-        }
+			//processor.IdentifyContours(image, trackBar1.Value, trackBar2.Value, trackBar3.Value, trackBar4.Value, trackBar5.Value, trackBar6.Value, chkBoxInvert.Checked, out gray, out color);
+			var colourFrom = new Hsv(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+			var colourTo = new Hsv(trackBar4.Value, trackBar5.Value, trackBar6.Value);
 
-        /// <summary>
-        /// Stop the camera and release resources when the form is closing
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void frmContours_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            CameraStreamCapture.Enabled = false;
+			var contours = ObjectFinder.FindAllContours(image, colourFrom, colourTo, chkBoxInvert.Checked);
+			using (MemStorage storage = new MemStorage())
+			{
+				foreach (var contour in contours.Where(contour => contour.Area >= 20))
+				{
+					myEye.Draw(contour.BoundingRectangle, new Bgr(Color.DeepPink), 2);
+				}
+			}
+			var roboEye = new Image<Hsv, byte>(image).InRange(colourFrom, colourTo);
+
+			pictBoxColor.Image = myEye.Bitmap;
+			pictBoxGray.Image = roboEye.Bitmap;
+		}
+
+		/// <summary>
+		/// Stop the camera and release resources when the form is closing
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void frmContours_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			CameraStreamCapture.Enabled = false;
 			capture = null;
-        }
+		}
 
 		private void trackBar6_Scroll(object sender, EventArgs e)
 		{
-
 		}
 
 		private void trackBar2_Scroll(object sender, EventArgs e)
 		{
-
 		}
 
 		private void trackBar1_Scroll(object sender, EventArgs e)
 		{
-
 		}
 
 		private void trackBar3_Scroll(object sender, EventArgs e)
 		{
-
 		}
 
 		private void trackBar4_Scroll(object sender, EventArgs e)
 		{
-
 		}
 
 		private void trackBar5_Scroll(object sender, EventArgs e)
 		{
-
 		}
-    }
+	}
 }
---
+++ ObjectFinderCore/ObjectFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace ObjectFinderCore
+{
+	public class ObjectFinder
+	{
+		public List<Contour<Point>> FindAllContours(Bitmap image, Hsv colourFrom, Hsv colourTo, bool invert)
+		{
+			var cuttedColoursImage = new Image<Hsv, byte>(image).InRange(colourFrom, colourTo);
+
+			if (invert)
+				cuttedColoursImage._Not();
+
+			var resultContours = new List<Contour<Point>>();
+			using (var storage = new MemStorage())
+			{
+				for (
+					var contours = cuttedColoursImage.FindContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_TC89_L1, RETR_TYPE.CV_RETR_EXTERNAL, storage);
+					contours != null;
+					contours = contours.HNext)
+				{
+					var currentContour = contours.ApproxPoly(contours.Perimeter * 0.015, storage);
+					resultContours.Add(currentContour);
+				}
+			}
+
+			return resultContours;
+		}
+
+		public List<Contour<Point>> FindContoursInRightSize(Bitmap image, Hsv colourFrom, Hsv colourTo, bool invert,
+			double areaFrom, double areaTo)
+		{
+			var allContours = FindAllContours(image, colourFrom, colourTo, invert);
+			return allContours.Where(contour => contour.Area >= areaFrom && contour.Area <= areaTo).ToList();
+		}
+	}
+}
